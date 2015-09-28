575dc28d5667d4be5c9c21e200263801b9ac7d88
Kartikaya Gupta <kgupta@mozilla.com>
2015-09-28 17:29:50 -0400

    Add regression range as a keyword

--- filter.php
+++ filter.php
@@ -189,6 +189,10 @@ function normalizeFieldList( $fieldString ) {
             if ($i + 1 < count( $words ) && $words[ $i + 1 ] == 'Story') {
                 $word .= ' ' . $words[ ++$i ];
             }
+        } else if ($word == 'Has' /* Regression Range */) {
+            if ($i + 2 < count( $words ) && $words[ $i + 1 ] == 'Regression' && $words[ $i + 2 ] == 'Range') {
+                $word .= ' ' . $words[ ++$i ] . ' ' . $words[ ++$i ];
+            }
         }
         if ($word == 'Ever Confirmed') {
             $word = 'Ever confirmed';
