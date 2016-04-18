f7a1c89e75dac390cb5282d0092a360523b3c704
Kartikaya Gupta <kgupta@mozilla.com>
2016-04-18 12:38:55 -0400

    Update filter to catch 'Restrict Comments'

--- filter.php
+++ filter.php
@@ -166,7 +166,8 @@ function normalizeFieldList( $fieldString ) {
             || $word == 'Crash' /* Signature */
             || $word == 'See' /* Also */
             || $word == 'Last' /* Resolved */
-            || $word == 'QA' /* Whiteboard */)
+            || $word == 'QA' /* Whiteboard */
+            || $word == 'Restrict' /* Comments */)
         {
             if ($i + 1 >= count( $words )) {
                 fail( 'Unrecognized field list (4): ' . print_r( $words, true ) );
