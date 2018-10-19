48bf5378265bbfc7afc4eb16dfe904ee97578470
Kartikaya Gupta <kats@pelorat.staktrace.com>
2018-10-19 15:16:30 +0000

    Increase newbugs.title to 1024 len

--- schemas/tables.sql
+++ schemas/tables.sql
@@ -67,7 +67,7 @@ CREATE TABLE `newbugs` (
     `stamp` datetime NOT NULL,
     `viewed` tinyint(1) NOT NULL DEFAULT 0,
     `reason` varchar(10) NOT NULL,
-    `title` varchar(255) NOT NULL,
+    `title` varchar(1024) NOT NULL,
     `author` varchar(255) NOT NULL,
     `description` mediumtext NOT NULL,
     PRIMARY KEY (`id`),
